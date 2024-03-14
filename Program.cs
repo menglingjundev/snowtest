60ca8c7c8e833daa425e9926c88dd4ae838047fc
menglingjundev <lingjun.meng@lixil.com>
2024-03-14 15:42:25 +0800

    Create test
    Create test

---
+++ Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var port = Environment.GetEnvironmentVariable("PORT");
+if (port == null)
+{
+    // デバッグ実行用
+    app.Run();
+}
+else
+{
+    //Cloud Run 用
+    var url = $"http://0.0.0.0:{port}";
+    var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
+
+    app.MapGet("/", () => $"Hello {target}!");
+
+    app.Run(url);
+}
